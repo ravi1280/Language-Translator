ad374d877b12d2f2637d40a68bd179d6ff290496
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-03-22 02:48:34 +0530

    Update

--- Translator/src/main/java/module-info.java
+++ Translator/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.translator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.example.translator to javafx.fxml;
     exports com.example.translator;
